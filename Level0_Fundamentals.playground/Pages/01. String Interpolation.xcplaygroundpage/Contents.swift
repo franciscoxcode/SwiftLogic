3f4845f2b99bd29f8bb87ebcc99269764bbb7803
Francisco Casillas <chiccasillas@gmail.com>
2025-07-21 11:47:52 -0600

    Create playground and practice String Interpolation

---
+++ Level0_Fundamentals.playground/Pages/01. String Interpolation.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//1.    Declara tu ciudad actual con var
+//2.    Declara tu país con let
+//3.    Imprime una frase con ambos
+//4.    Cambia el valor de la ciudad
+//5.    Imprime de nuevo para ver el cambio
+
+var city = "CDMX"
+let country = "México"
+
+print("My city is \(city) in \(country)")
+
+city = "Guanajuato"
+
+print("I live in \(city), \(country)")
