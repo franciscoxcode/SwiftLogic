23a401ea6fe427d0d48905713893881086349d41
Francisco Casillas <chiccasillas@gmail.com>
2025-07-24 10:18:46 -0600

    Integer to String conversions

---
+++ Level0_Fundamentals.playground/Pages/02. Type casting.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let age: Int = 41
+let ageAsString: String = String(age)
+
+print(age)
+print(type(of: age))
+print(ageAsString)
+print(type(of: ageAsString))
