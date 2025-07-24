44c928aaf3084ee3c30c9075903e368f76bc1446
Francisco Casillas <chiccasillas@gmail.com>
2025-07-24 10:25:23 -0600

    Double to Integer conversions

--- Level0_Fundamentals.playground/Pages/02. Type casting.xcplaygroundpage/Contents.swift
+++ Level0_Fundamentals.playground/Pages/02. Type casting.xcplaygroundpage/Contents.swift
@@ -2,10 +2,19 @@
 
 import Foundation
 
-let age: Int = 41
-let ageAsString: String = String(age)
+//let age: Int = 41
+//let ageAsString: String = String(age)
+//
+//print(age)
+//print(type(of: age))
+//print(ageAsString)
+//print(type(of: ageAsString))
 
-print(age)
-print(type(of: age))
-print(ageAsString)
-print(type(of: ageAsString))
+let height: Double = 1.78
+let heightAsInt: Int = Int(height)
+
+print(height)
+print(type(of: height))
+
+print(heightAsInt)
+print(type(of: heightAsInt))
