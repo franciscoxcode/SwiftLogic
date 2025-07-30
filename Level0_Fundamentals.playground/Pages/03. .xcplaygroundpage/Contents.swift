f244e98d7f999b18a51b4636d7e7f8d752052a1b
Francisco Casillas <chiccasillas@gmail.com>
2025-07-30 10:45:34 -0600

    practice: safely retrieve elements from an array

--- Level0_Fundamentals.playground/Pages/03. .xcplaygroundpage/Contents.swift
+++ Level0_Fundamentals.playground/Pages/03. .xcplaygroundpage/Contents.swift
@@ -12,15 +12,25 @@ let letras = ["A", "B", "C"]
 // 2. Imprime el tercer elemento solo si existe.
 // 3. Intenta acceder al índice 5 de forma segura.
 
-print(letras[1])
+//print(letras[1])
+//
+//if(letras.count > 2){
+//    print(letras[2])
+//}
+//if(letras.count >= 5){
+//    print(letras[5])
+//}
+//
+//if letras.indices.contains(5){
+//    print(letras[5])
+//}
 
-if(letras.count > 2){
-    print(letras[2])
-}
-if(letras.count >= 5){
-    print(letras[5])
-}
+let planetas = ["Mercurio", "Venus", "Tierra", "Marte"]
 
-if letras.indices.contains(5){
-    print(letras[5])
-}
+// 1. Imprime el primer planeta usando índices.
+// 2. Imprime el último planeta usando `.count`.
+// 3. Imprime el último planeta usando `.last` de forma segura.
+
+print(planetas[0])
+print(planetas[planetas.count - 1])
+print(planetas.last!)
