14ef2bf6eb78ca9da4be92c4e2a4db6bdbad7ea2
Francisco Casillas <chiccasillas@gmail.com>
2025-08-21 11:00:48 -0600

    guard let to unwrap optionals

--- Level0_Fundamentals.playground/Pages/04. Unwrapping optionals.xcplaygroundpage/Contents.swift
+++ Level0_Fundamentals.playground/Pages/04. Unwrapping optionals.xcplaygroundpage/Contents.swift
@@ -2,10 +2,23 @@
 
 import Foundation
 
-var phone: String? = "555-1234"
+//var phone: String? = "555-1234"
+//
+//if let unwrappedPhone = phone {
+//    print("Tu número es \(unwrappedPhone).")
+//} else {
+//    print("No hay número registrado.")
+//}
 
-if let unwrappedPhone = phone {
-    print("Tu número es \(unwrappedPhone).")
-} else {
-    print("No hay número registrado.")
+func printUsername(_ username: String?) {
+    guard let unwrappedUsername = username else {
+        print("Sin usuario")
+        return
+    }
+    
+    print("Usuario: \(unwrappedUsername)")
 }
+
+printUsername("Joel")
+printUsername(nil)
+
