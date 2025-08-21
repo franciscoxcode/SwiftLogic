6612399537e29a448ee0a9a71e7c84b19af8453b
Francisco Casillas <chiccasillas@gmail.com>
2025-08-21 11:12:46 -0600

    practice: if let, guard let unwrapping with structs and functions

--- Level0_Fundamentals.playground/Pages/04. Unwrapping optionals.xcplaygroundpage/Contents.swift
+++ Level0_Fundamentals.playground/Pages/04. Unwrapping optionals.xcplaygroundpage/Contents.swift
@@ -10,15 +10,53 @@ import Foundation
 //    print("No hay número registrado.")
 //}
 
-func printUsername(_ username: String?) {
-    guard let unwrappedUsername = username else {
-        print("Sin usuario")
+//func printUsername(_ username: String?) {
+//    guard let unwrappedUsername = username else {
+//        print("Sin usuario")
+//        return
+//    }
+//    
+//    print("Usuario: \(unwrappedUsername)")
+//}
+//
+//printUsername("Joel")
+//printUsername(nil)
+
+struct Profile {
+    var name: String?
+    var email: String?
+}
+
+let profile1 = Profile(name: "Benjamin", email: "what@gmail.com")
+let profile2 = Profile(name: nil, email: "ye@mail.com")
+
+func printProfile(_ profile: Profile){
+    if let name = profile.name {
+        if let email = profile.email {
+            print("Bienvenido, \(name). Tu email es \(email)")
+        } else {
+            print("Faltan datos del perfil.")
+        }
+    } else {
+        print("Faltan datos del perfil.")
+    }
+}
+
+func printProf(_ profile: Profile){
+    guard let name = profile.name else {
+        print("Hace falta nombre.")
+        return
+    }
+    guard let email = profile.email else {
+        print("Hace falta email")
         return
     }
     
-    print("Usuario: \(unwrappedUsername)")
+    print("Bienvenido, \(name). Tu email es \(email)")
 }
 
-printUsername("Joel")
-printUsername(nil)
+printProfile(profile1)
+printProf(profile1)
 
+printProfile(profile2)
+printProf(profile2)
