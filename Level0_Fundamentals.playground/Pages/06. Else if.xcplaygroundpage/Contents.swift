2a618d2b2c948ffc5d5eb0b60a11da659c5c5e05
Francisco Casillas <chiccasillas@gmail.com>
2025-08-21 13:28:32 -0600

    practice: if, else, else if and naming practice for functions

---
+++ Level0_Fundamentals.playground/Pages/06. Else if.xcplaygroundpage/Contents.swift
@@ -0,0 +1,56 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//func isAdult(_ age: Int) {
+//    if age >= 18 {
+//        print("Mayor de edad")
+//    } else {
+//        print("Menor de edad")
+//    }
+//}
+//
+//isAdult(18)
+
+//func printSatisfactionLevel(for grade: Int) {
+//    if grade >= 90 {
+//        print("Excelente")
+//    } else if grade >= 70 {
+//        print("Bien")
+//    } else if grade >= 50 {
+//        print("Suficiente")
+//    } else {
+//        print("Reprobado")
+//    }
+//}
+//
+//printSatisfactionLevel(for: 100)
+
+//func describeNumber(_ number: Int) {
+//    if number > 0 {
+//        print("Positivo")
+//    } else if number == 0 {
+//        print("Es cero")
+//    } else {
+//        print("Negativo")
+//    }
+//}
+//
+//describeNumber(10)
+
+//let username = "Max"
+//let password = "1234"
+//
+//func accessGranted(to user: String, _ pass: String) {
+//    if username == user && password == pass {
+//        print("Acceso concedido")
+//    } else if username == user {
+//        print("Contraseña incorrecta")
+//    } else {
+//        print("Usuario no encontrado")
+//    }
+//}
+//
+//accessGranted(to: "Max", "1234")
+//accessGranted(to: "Max", "222")
+//accessGranted(to: "John", "333")
