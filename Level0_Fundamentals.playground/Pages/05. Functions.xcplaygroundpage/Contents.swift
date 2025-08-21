79ff4a089e88d35df5e76fbba46d3a2ecf3efde2
Francisco Casillas <chiccasillas@gmail.com>
2025-08-21 12:55:33 -0600

    practice: functions with return value, void, external and internal names

---
+++ Level0_Fundamentals.playground/Pages/05. Functions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//func sayHi() {
+//    print("Hola desde Swift!")
+//}
+//
+//sayHi()
+
+//func greet(_ name: String) {
+//    print("Hello, \(name)")
+//}
+//
+//greet("Juan")
+
+//func square(_ number: Int) {
+//    print("El cuadrado es \(number*number)")
+//}
+//
+//square(5)
+
+//func sum(_ num1: Int, _ num2: Int) -> Int {
+//    return num1 + num2
+//}
+//
+//print(sum(3, 5))
+
+func introduce(person name: String){
+    print("Mucho gusto, soy \(name)")
+}
+
+introduce(person: "James")
